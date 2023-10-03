f3577c70974ee4dfa552ed95f6823433803a9379
Kennedy Martins <kenybustamante2@hotmail.com>
2023-10-03 03:38:37 +0000

    database config

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors")
+const database = require("./database/sqlite")
 const express = require("express")
 const routes = require("./routes")
 const AppError = require("./utils/AppError")
@@ -8,6 +9,8 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+database()
 
 app.use((error, request, response, next ) => {
   if(error instanceof AppError) {
