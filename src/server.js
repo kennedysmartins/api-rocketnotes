e16be7e6e5831ab2b9de50a4d1ca55535387aa78
Kennedy Martins <kenybustamante2@hotmail.com>
2023-10-03 00:08:31 +0000

    using AppError

--- src/server.js
+++ src/server.js
@@ -1,10 +1,27 @@
+require("express-async-errors")
 const express = require("express")
 const routes = require("./routes")
+const AppError = require("./utils/AppError")
+
 
 const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error, request, response, next ) => {
+  if(error instanceof AppError) {
+    return response.status(error.status).json({
+      status:"error",
+      message:error.message
+    });
+  }
+  console.log(error)
+  return response.status(500).json({
+    status:"error",
+      message:"Internal Server error"
+  })
+})
 
 const PORT = 3333
 
