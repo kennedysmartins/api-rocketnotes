80dd0e6ff3c73e59268c183ec9756ef5efc63ed9
kennedysmartins <kennedysmartins@hotmail.com>
2023-10-04 17:45:20 -0300

    changes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors")
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 const express = require("express")
 const routes = require("./routes")
 const AppError = require("./utils/AppError")
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.use(routes)
 
-database()
+migrationsRun()
 
 app.use((error, request, response, next ) => {
   if(error instanceof AppError) {
